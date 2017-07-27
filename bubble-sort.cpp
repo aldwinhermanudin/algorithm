77d16451d5e8c1480b8fe8373d044f148f6b5580
Aldwin Hermanudin <aldwinakbar@gmail.com>
2017-07-27 23:30:38 +0700

    optimize

--- bubble-sort.cpp
+++ bubble-sort.cpp
@@ -10,13 +10,11 @@ int main(){
 	int array_size = 12;
 	int array_unsorted[array_size] = {5,45,1,3,5,-1,0,13,41,17,31,33}; 
 	
-	for(int i = 0; i < array_size; i++){
-				
-		if(array_unsorted[i+1] < array_unsorted[i]){
-			Swap(array_unsorted+i, array_unsorted+(i+1));
+	for(int i = 1; i < array_size; i++){
+		if(array_unsorted[i-1] > array_unsorted[i]){
 			for(int j = i; j > 0; j--){
-				if(array_unsorted[j] < array_unsorted[j-1]){
-					Swap(array_unsorted+j, array_unsorted+(j-1));
+				if(array_unsorted[j-1] > array_unsorted[j]){
+					Swap(array_unsorted+(j-1), array_unsorted+j);
 				}
 			}
 		}
